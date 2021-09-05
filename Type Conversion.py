40ca0d4304f5aeecc37cd173a380f3f5309e2494
PariseneniSaiTeja <89776915+PariseneniSaiTeja@users.noreply.github.com>
2021-09-05 11:44:31 +0530

    Add files via upload

---
+++ Type Conversion.py
@@ -0,0 +1,7 @@
+weight=input("pounds:")
+kg= int(weight) / 2.205
+print("conversion from pounds to kg is",+ kg)
+
+birth_year=input("birth year:")
+age= 2021 - int(birth_year)
+print("your age is", +age )
